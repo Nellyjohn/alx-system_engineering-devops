aa8b2c503be3bbd8a1fa298ce4a2b3935c1bcfd6
veeviyan <okpala_vivian@yahoo.com>
2024-03-25 10:37:27 +0100

    Using Puppet, create a file in /tmp.

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,16 @@
+# creates a file in /tmp
+# Requirements:
+#
+# File path is /tmp/school
+# File permission is 0744
+# File owner is www-data
+# File group is www-data
+# File contains I love Puppet
+
+file {'/tmp/school':
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,1 @@
+#**Solutions to 0x0A-configuration_management project**
